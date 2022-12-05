94c82d8e79b3177b5504dc9748dba5fda3d2ede7
James Lewis <byerly0503@gmail.com>
2022-12-05 17:21:17 -0600

    ignore this file ...

--- code/include/ca_cert.h
+++
@@ -1,7 +0,0 @@
-// Let's Encrypt's CA Cert
-const char* lets_encrypt_ca_cert = \
-    "-----BEGIN CERTIFICATE-----\n" \
-// cert blah blah goes here
-// https://letsencrypt.org/certificates/
-    "-----END CERTIFICATE-----\n";
-    
